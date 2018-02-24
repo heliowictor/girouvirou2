811b864dbb2b7c06d502eec2e1784665ae277db9
Hélio Victor <heliowictor@hotmail.com>
2018-02-24 02:06:50 -0300

    Adcionando o jogo girou virou

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1519448056,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/sprite-sheet0.png",
+		"images/inicio.png",
+		"images/sprite2-sheet0.png",
+		"images/btn_iniciar-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/roleta-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/girar-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/sprite7-sheet0.png",
+		"images/seta-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/com_som-sheet0.png",
+		"images/sem_som-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"media/roleta giri.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
